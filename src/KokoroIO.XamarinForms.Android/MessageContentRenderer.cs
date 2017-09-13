0983fbe47a00349700abfa4decfc0e46d203c121
pgrho <shipwreck@live.jp>
2017-09-13 23:13:09 +0900

    Added MessageContent and its renderer

---
+++ src/KokoroIO.XamarinForms.Android/MessageContentRenderer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using Android.Text;
+using Android.Widget;
+using KokoroIO.XamarinForms.Droid;
+using KokoroIO.XamarinForms.ViewModels;
+using KokoroIO.XamarinForms.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MessageContent), typeof(MessageContentRenderer))]
+
+namespace KokoroIO.XamarinForms.Droid
+{
+    public sealed class MessageContentRenderer : ViewRenderer<MessageContent, TextView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<MessageContent> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    SetNativeControl(new TextView(Context));
+                }
+                UpdateBlocks(Control);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == MessageContent.BlocksProperty.PropertyName)
+            {
+                UpdateBlocks(Control);
+            }
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        private WeakReference<INotifyCollectionChanged> _Blocks;
+
+        private void UpdateBlocks(TextView textView)
+        {
+            if (_Blocks != null && _Blocks.TryGetTarget(out var v))
+            {
+                v.CollectionChanged -= Blocks_CollectionChanged;
+            }
+
+            var bs = Element.Blocks;
+
+            if (bs is INotifyCollectionChanged cc)
+            {
+                cc.CollectionChanged += Blocks_CollectionChanged;
+                _Blocks = new WeakReference<INotifyCollectionChanged>(cc);
+            }
+            else
+            {
+                _Blocks = null;
+            }
+
+            if (bs == null)
+            {
+                textView.Text = "";
+                return;
+            }
+
+            var builder = new SpannableStringBuilder();
+
+            foreach (var b in bs)
+            {
+                if (builder.Length() > 0)
+                {
+                    builder.Append(System.Environment.NewLine);
+                    builder.Append(System.Environment.NewLine);
+                }
+
+                if (b is MessageBlock mb)
+                {
+                    foreach (var s in mb.Spans)
+                    {
+                        AppendSpan(s, builder);
+                    }
+                }
+                else if (b is ListMessageBlock lmb)
+                {
+                    foreach (var li in lmb.Items)
+                    {
+                        if (builder.Length() > 0)
+                        {
+                            builder.Append(System.Environment.NewLine);
+                        }
+
+                        builder.Append("    * ");
+                        foreach (var s in li.Spans)
+                        {
+                            AppendSpan(s, builder);
+                        }
+                    }
+                }
+                else
+                {
+                    builder.Append(b.ToString());
+                }
+            }
+
+            textView.TextFormatted = builder;
+        }
+
+        private static void AppendSpan(MessageSpan s, SpannableStringBuilder builder)
+        {
+            builder.Append(s.Text);
+        }
+
+        private void Blocks_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+            => UpdateBlocks(Control);
+    }
+}
---
+++ src/KokoroIO.XamarinForms/Views/MessageContent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using KokoroIO.XamarinForms.ViewModels;
+using Xamarin.Forms;
+
+namespace KokoroIO.XamarinForms.Views
+{
+    public sealed class MessageContent : View
+    {
+        public static readonly BindableProperty BlocksProperty = BindableProperty.Create(nameof(Blocks), typeof(IEnumerable<MessageBlockBase>), typeof(MessageContent));
+
+        public IEnumerable<MessageBlockBase> Blocks
+        {
+            get => GetValue(BlocksProperty) as IEnumerable<MessageBlockBase>;
+            set => SetValue(BlocksProperty, value);
+        }
+    }
+}
