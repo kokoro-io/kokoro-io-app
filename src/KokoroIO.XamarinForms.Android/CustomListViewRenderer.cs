4768e0484dd4f626df2d427894724fb13944c1de
pgrho <shipwreck@live.jp>
2017-09-21 19:31:56 +0900

    Add Droid.CustomListViewRenderer to avoid dispatch events

---
+++ src/KokoroIO.XamarinForms.Android/CustomListViewRenderer.cs
@@ -0,0 +1,75 @@
+using Android.Views;
+using KokoroIO.XamarinForms.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ListView), typeof(CustomListViewRenderer))]
+
+namespace KokoroIO.XamarinForms.Droid
+{
+    public sealed class CustomListViewRenderer : ListViewRenderer
+    {
+        public override bool DispatchDragEvent(DragEvent e)
+        {
+            if (Element.InputTransparent)
+            {
+                return false;
+            }
+            return base.DispatchDragEvent(e);
+        }
+
+        protected override bool DispatchGenericFocusedEvent(MotionEvent e)
+        {
+            if (Element.InputTransparent)
+            {
+                return false;
+            }
+            return base.DispatchGenericFocusedEvent(e);
+        }
+
+        public override bool DispatchGenericMotionEvent(MotionEvent e)
+        {
+            if (Element.InputTransparent)
+            {
+                return false;
+            }
+            return base.DispatchGenericMotionEvent(e);
+        }
+
+        protected override bool DispatchGenericPointerEvent(MotionEvent e)
+        {
+            if (Element.InputTransparent)
+            {
+                return false;
+            }
+            return base.DispatchGenericPointerEvent(e);
+        }
+
+        public override bool DispatchKeyEvent(KeyEvent e)
+        {
+            if (Element.InputTransparent)
+            {
+                return false;
+            }
+            return base.DispatchKeyEvent(e);
+        }
+
+        protected override bool DispatchHoverEvent(MotionEvent e)
+        {
+            if (Element.InputTransparent)
+            {
+                return false;
+            }
+            return base.DispatchHoverEvent(e);
+        }
+
+        public override bool DispatchTouchEvent(MotionEvent e)
+        {
+            if (Element.InputTransparent)
+            {
+                return false;
+            }
+            return base.DispatchTouchEvent(e);
+        }
+    }
+}
