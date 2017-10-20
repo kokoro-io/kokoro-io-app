33336a0d3a19a806672089f810fca251eaa20a5a
pgrho <shipwreck@live.jp>
2017-10-21 00:03:45 +0900

    Inserted channel group nodes

--- src/KokoroIO.XamarinForms/Views/ChannelsView.xaml.cs
+++ src/KokoroIO.XamarinForms/Views/ChannelsView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -11,12 +12,204 @@ namespace KokoroIO.XamarinForms.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChannelsView : ContentView
     {
+        private interface ITreeNodeParent : IEnumerable<TreeNode>
+        {
+            void Add(TreeNode item);
+
+            void Remove(TreeNode item);
+        }
+
+        private sealed class KindNode : ITreeNodeParent
+        {
+            private readonly List<TreeNode> _Children = new List<TreeNode>();
+
+            public string Title { get; set; }
+            public ChannelKind Kind { get; set; }
+
+            public void Add(TreeNode item)
+            {
+                _Children.Add(item);
+            }
+
+            public void Remove(TreeNode item)
+            {
+                _Children.Remove(item);
+            }
+
+            public IEnumerator<TreeNode> GetEnumerator()
+                => _Children.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator()
+                => _Children.GetEnumerator();
+        }
+
+        private abstract class TreeNode : ObservableObject, IDisposable
+        {
+            protected readonly ChannelsView Control;
+
+            protected TreeNode(ChannelsView control, GroupNode parent, string name)
+            {
+                Control = control;
+                Parent = parent;
+                Name = name;
+            }
+
+            internal GroupNode Parent { get; }
+
+            public int Depth => (Parent?.Depth ?? -1) + 1;
+
+            public string Name { get; }
+            public abstract string FullName { get; }
+
+            public abstract bool IsGroup { get; }
+
+            public abstract bool IsSelected { get; }
+
+            public abstract bool IsArchived { get; }
+
+            public abstract int UnreadCount { get; }
+
+            public virtual void Dispose()
+            {
+                Parent?.Remove(this);
+            }
+        }
+
+        private sealed class ChannelNode : TreeNode
+        {
+            internal ChannelViewModel Channel;
+
+            public ChannelNode(ChannelsView control, ChannelViewModel channel, GroupNode parent, string name)
+                : base(control, parent, name)
+            {
+                Channel = channel;
+                Channel.PropertyChanged += Channel_PropertyChanged;
+
+                Parent?.Add(this);
+            }
+
+            public override bool IsGroup => false;
+
+            public override bool IsSelected => Channel.IsSelected;
+            public override bool IsArchived => Channel.IsArchived;
+
+            public override int UnreadCount => Channel.UnreadCount;
+
+            public override string FullName => Parent?.FullName + Name;
+
+            private void Channel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+            {
+                switch (e.PropertyName)
+                {
+                    case nameof(Channel.IsArchived):
+                    case nameof(Channel.DisplayName):
+                        Control.Remove(Channel);
+                        Control.Add(Channel);
+                        break;
+
+                    case nameof(Channel.IsSelected):
+                    case nameof(Channel.UnreadCount):
+                        OnPropertyChanged(e.PropertyName);
+                        break;
+                }
+            }
+
+            public override void Dispose()
+            {
+                base.Dispose();
+                Channel.PropertyChanged -= Channel_PropertyChanged;
+            }
+        }
+
+        private sealed class GroupNode : TreeNode, ITreeNodeParent
+        {
+            private readonly List<TreeNode> _Children;
+
+            public GroupNode(ChannelsView control, GroupNode parent, string name)
+                : base(control, parent, name)
+            {
+                _Children = new List<TreeNode>(4);
+
+                Parent?.Add(this);
+            }
+
+            public override string FullName => Parent?.FullName + Name + "/";
+
+            public override bool IsGroup => true;
+
+            public override bool IsSelected => false;
+
+            private int _UnreadCount;
+
+            public override int UnreadCount => _UnreadCount;
+
+            private void SetUnreadCount()
+                => SetProperty(ref _UnreadCount, _Children.Sum(c => c.UnreadCount), nameof(UnreadCount));
+
+            private bool _IsArchived;
+
+            public override bool IsArchived => _IsArchived;
+
+            private void SetIsArchived()
+                => SetProperty(ref _IsArchived, _Children.All(c => c.IsArchived), nameof(IsArchived));
+
+            public void Add(TreeNode item)
+            {
+                _Children.Add(item);
+                SetUnreadCount();
+                SetIsArchived();
+                item.PropertyChanged += Item_PropertyChanged;
+            }
+
+            public void Remove(TreeNode item)
+            {
+                item.PropertyChanged -= Item_PropertyChanged;
+                _Children.Remove(item);
+                SetUnreadCount();
+                SetIsArchived();
+            }
+
+            private void Item_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+            {
+                switch (e.PropertyName)
+                {
+                    case nameof(UnreadCount):
+                        SetUnreadCount();
+                        break;
+
+                    case nameof(IsArchived):
+                        SetIsArchived();
+                        break;
+                }
+            }
+
+            public override void Dispose()
+            {
+                base.Dispose();
+
+                foreach (var e in _Children)
+                {
+                    e.PropertyChanged -= Item_PropertyChanged;
+                }
+
+                _Children.Clear();
+            }
+
+            public IEnumerator<TreeNode> GetEnumerator()
+                => _Children.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator()
+                => _Children.GetEnumerator();
+        }
+
+        private DataTemplate ItemTemplate;
+
         public ChannelsView()
         {
             InitializeComponent();
-        }
 
-        public DataTemplate ItemTemplate { get; set; }
+            ItemTemplate = (DataTemplate)Resources["channelListViewItemTemplate"];
+        }
 
         #region ItemsSource
 
@@ -74,60 +267,14 @@ namespace KokoroIO.XamarinForms.Views
 
         private bool OnItemAdding(NotifyCollectionChangedEventArgs e)
         {
-            if (e.NewItems == null)
+            if (e.NewItems == null || tableView.Root == null)
             {
                 return false;
             }
-            var nsi = e.NewStartingIndex >= 0 ? e.NewStartingIndex : ((IList)ItemsSource).IndexOf(e.NewItems[0]);
-            if (nsi < 0)
-            {
-                return false;
-            }
-            TableSection s = null;
-            int ni = 0;
-            var prev = ItemsSource.ElementAtOrDefault(nsi - 1);
 
             foreach (ChannelViewModel n in e.NewItems)
             {
-                if (prev?.Kind == n.Kind
-                    && prev?.IsArchived == n.IsArchived)
-                {
-                    if (s == null)
-                    {
-                        s = tableView.Root.FirstOrDefault(ts => ts.Any(c => c.BindingContext == prev));
-                        if (s == null)
-                        {
-                            s = new TableSection(n.KindName);
-                            tableView.Root.Add(s);
-                            ni = 0;
-                        }
-                        else
-                        {
-                            ni = s.IndexOf(s.FirstOrDefault(c => c.BindingContext == prev));
-                            if (ni < 0)
-                            {
-                                ni = s.Count;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    var os = s;
-
-                    s = new TableSection(n.KindName);
-
-                    tableView.Root.Insert(os == null ? tableView.Root.Count : tableView.Root.IndexOf(os) + 1, s);
-                    ni = 0;
-                }
-
-                var cell = ItemTemplate.CreateContent() as Cell;
-                cell.BindingContext = n;
-                cell.Tapped += Cell_Tapped;
-
-                s.Insert(ni++, cell);
-
-                prev = n;
+                Add(n);
             }
 
             return true;
@@ -135,40 +282,14 @@ namespace KokoroIO.XamarinForms.Views
 
         private bool OnItemRemoving(NotifyCollectionChangedEventArgs e)
         {
-            if (e.OldItems == null)
+            if (e.OldItems == null || tableView.Root == null)
             {
                 return false;
             }
 
             foreach (ChannelViewModel o in e.OldItems)
             {
-                var removed = false;
-                foreach (var s in tableView.Root)
-                {
-                    foreach (var c in s)
-                    {
-                        if (c.BindingContext == o)
-                        {
-                            c.Tapped -= Cell_Tapped;
-                            s.Remove(c);
-                            if (!s.Any())
-                            {
-                                tableView.Root.Remove(s);
-                            }
-                            removed = true;
-                            break;
-                        }
-                    }
-                    if (removed)
-                    {
-                        break;
-                    }
-                }
-
-                if (!removed)
-                {
-                    return false;
-                }
+                Remove(o);
             }
 
             return true;
@@ -178,23 +299,23 @@ namespace KokoroIO.XamarinForms.Views
         {
             var root = new TableRoot();
 
+            foreach (var kn in new KindNode[]
+            {
+                new KindNode(){ Title="Public Channels", Kind= ChannelKind.PublicChannel },
+                new KindNode(){ Title="Private Channels", Kind= ChannelKind.PrivateChannel },
+                new KindNode(){ Title="Direct Messages", Kind= ChannelKind.DirectMessage }
+            })
+            {
+                root.Add(new TableSection(kn.Title) { BindingContext = kn });
+            }
+
             var src = ItemsSource;
 
             if (src != null)
             {
                 foreach (var c in src)
                 {
-                    var sec = root.LastOrDefault() as TableSection;
-                    if (sec?.Title != c.KindName)
-                    {
-                        sec = new TableSection(c.KindName);
-                        root.Add(sec);
-                    }
-
-                    var cell = ItemTemplate.CreateContent() as Cell;
-                    cell.BindingContext = c;
-                    cell.Tapped += Cell_Tapped;
-                    sec.Add(cell);
+                    Add(c, root);
                 }
             }
 
@@ -205,6 +326,7 @@ namespace KokoroIO.XamarinForms.Views
                     foreach (var c in s)
                     {
                         c.Tapped -= Cell_Tapped;
+                        (c.BindingContext as IDisposable)?.Dispose();
                     }
                 }
             }
@@ -212,17 +334,142 @@ namespace KokoroIO.XamarinForms.Views
             tableView.Root = root;
         }
 
-        private void Cell_Tapped(object sender, System.EventArgs e)
+        private void Add(ChannelViewModel item, TableRoot root = null)
         {
-            var lv = sender as Cell;
-
-            var item = lv?.BindingContext as ChannelViewModel;
-            if (item == null)
+            root = root ?? tableView.Root;
+            if (root == null)
             {
                 return;
             }
 
-            item.Application.SelectedChannel = item;
+            var sec = root.FirstOrDefault(s => (s.BindingContext as KindNode)?.Kind == item.Kind);
+
+            if (sec == null)
+            {
+                var kn = new KindNode() { Title = item.Kind.ToString(), Kind = item.Kind };
+                root.Add(new TableSection(kn.Title) { BindingContext = kn });
+            }
+            var kind = (KindNode)sec.BindingContext;
+
+            var path = item.ChannelName.Split('/');
+            var parent = sec.BindingContext as ITreeNodeParent;
+
+            var si = 0;
+
+            for (var i = 0; i < path.Length; i++)
+            {
+                var pg = parent as GroupNode;
+                var name = path[i];
+                bool isGroup = i < path.Length - 1;
+                TreeNode newNode = null;
+                if (isGroup)
+                {
+                    var g = parent.OfType<GroupNode>().FirstOrDefault(n => n.Name == name);
+                    if (g == null)
+                    {
+                        newNode = g = new GroupNode(this, pg, name);
+                    }
+                    parent = g;
+                }
+                else
+                {
+                    var c = parent.OfType<ChannelNode>().FirstOrDefault(n => n.Channel == item);
+                    if (c == null)
+                    {
+                        newNode = c = new ChannelNode(this, item, pg, name);
+                    }
+                }
+
+                var pd = pg?.Depth ?? -1;
+                var siblings = sec.Skip(si).TakeWhile(cell => ((TreeNode)cell.BindingContext).Depth > pd);
+
+                if (newNode == null)
+                {
+                    if (isGroup)
+                    {
+                        si = sec.IndexOf(siblings.First(cell => cell.BindingContext == parent)) + 1;
+                    }
+                }
+                else
+                {
+                    var next = siblings.Where(cell => cell.BindingContext is TreeNode n
+                                                    && n.Depth == pd + 1
+                                                    && n.IsGroup == isGroup
+                                                    && newNode.Name.CompareTo(n.Name) < 0).FirstOrDefault()
+                                ?? sec.Skip(si).SkipWhile(cell => ((TreeNode)cell.BindingContext).Depth > pd).FirstOrDefault();
+
+                    if (next == null)
+                    {
+                        si = sec.Count;
+                    }
+                    else
+                    {
+                        si = sec.IndexOf(next);
+                    }
+
+                    var vc = ItemTemplate.CreateContent() as Cell;
+                    vc.BindingContext = newNode;
+                    vc.Tapped += Cell_Tapped;
+
+                    sec.Insert(si++, vc);
+
+                    if (pg == null)
+                    {
+                        kind.Add(newNode);
+                    }
+                }
+            }
+        }
+
+        private void Remove(ChannelViewModel item)
+        {
+            var root = tableView.Root;
+            if (root == null)
+            {
+                return;
+            }
+
+            var sec = root.FirstOrDefault(s => (s.BindingContext as KindNode)?.Kind == item.Kind);
+
+            if (sec == null)
+            {
+                return;
+            }
+
+            var cell = sec.FirstOrDefault(c => (c.BindingContext as ChannelNode)?.Channel == item);
+            var node = cell.BindingContext as TreeNode;
+            while (node != null)
+            {
+                if (cell != null)
+                {
+                    cell.Tapped -= Cell_Tapped;
+                    sec.Remove(cell);
+                }
+
+                node.Dispose();
+
+                if (node.Parent == null || node.Parent.Any())
+                {
+                    break;
+                }
+                node = node.Parent;
+                cell = sec.FirstOrDefault(c => c.BindingContext == node);
+            }
+        }
+
+        private void Cell_Tapped(object sender, System.EventArgs e)
+        {
+            var lv = sender as Cell;
+
+            if (lv.BindingContext is ChannelNode c)
+            {
+                var item = c.Channel;
+                item.Application.SelectedChannel = item;
+            }
+            else if (lv.BindingContext is GroupNode g)
+            {
+
+            }
         }
 
         #endregion ItemsSource
---
+++ src/KokoroIO.XamarinForms/Views/GridLengthConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace KokoroIO.XamarinForms.Views
+{
+    public sealed class GridLengthConverter : IValueConverter
+    {
+        public double Coefficient { get; set; } = 1;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = (value as IConvertible)?.ToDouble(culture) ?? 0;
+
+            return new GridLength(v * Coefficient);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => ((GridLength)value).Value / Coefficient;
+    }
+}
