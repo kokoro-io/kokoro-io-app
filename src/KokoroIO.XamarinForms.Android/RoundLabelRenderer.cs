fd88e30361ac9016c19e49ed529688610174b51e
pgrho <shipwreck@live.jp>
2017-10-27 00:00:07 +0900

    Apply corner radius to unread count

---
+++ src/KokoroIO.XamarinForms.Android/RoundLabelRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using KokoroIO.XamarinForms.Droid;
+using KokoroIO.XamarinForms.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(RoundLabel), typeof(RoundLabelRenderer))]
+
+namespace KokoroIO.XamarinForms.Droid
+{
+    public class RoundLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement == null)
+            {
+                return;
+            }
+
+            if (Control != null)
+            {
+                Control.Invalidate();
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            switch (e.PropertyName)
+            {
+                case nameof(Label.Text):
+                case nameof(RoundLabel.BackgroundColor):
+                case nameof(RoundLabel.BorderColor):
+                case nameof(RoundLabel.BorderWidth):
+                case nameof(RoundLabel.CornerRadius):
+                    Control.Invalidate();
+                    break;
+            }
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+
+            var label = Element as RoundLabel;
+
+            if (label != null)
+            {
+                var background = new Android.Graphics.Drawables.GradientDrawable();
+                background.SetCornerRadius((int)label.CornerRadius);
+                background.SetStroke((int)label.BorderWidth, label.BorderColor.ToAndroid());
+                background.SetColor(label.BackgroundColor.ToAndroid());
+                SetBackgroundColor(Android.Graphics.Color.Transparent);
+                SetBackgroundDrawable(background);
+            }
+        }
+    }
+}
---
+++ src/KokoroIO.XamarinForms/Views/RoundLabel.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+
+namespace KokoroIO.XamarinForms.Views
+{
+    public class RoundLabel : Label
+    {
+        public static readonly BindableProperty BorderColorProperty =
+            BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(RoundLabel), Color.Transparent);
+
+        public static readonly BindableProperty BorderWidthProperty =
+            BindableProperty.Create(nameof(BorderWidth), typeof(float), typeof(RoundLabel), 0f);
+
+        public static readonly BindableProperty CornerRadiusProperty =
+            BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(RoundLabel), 0f);
+
+        public Color BorderColor
+        {
+            get => (Color)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
+        }
+
+        public float BorderWidth
+        {
+            get => (float)GetValue(BorderWidthProperty);
+            set => SetValue(BorderWidthProperty, value);
+        }
+
+        public float CornerRadius
+        {
+            get => (float)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+    }
+}
