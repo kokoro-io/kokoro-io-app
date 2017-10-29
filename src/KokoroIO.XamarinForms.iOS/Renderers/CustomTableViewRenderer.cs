f53cb65240b46adb38d8d4b036ab3751b7178905
pgrho <shipwreck@live.jp>
2017-10-29 22:35:10 +0900

    Remove separator line of TableView in iOS #46

---
+++ src/KokoroIO.XamarinForms.iOS/Renderers/CustomTableViewRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using KokoroIO.XamarinForms.iOS.Renderers;
+using KokoroIO.XamarinForms.Views;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomTableView), typeof(CustomTableViewRenderer))]
+
+namespace KokoroIO.XamarinForms.iOS.Renderers
+{
+    public class CustomTableViewRenderer : TableViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<TableView> e)
+        {
+            base.OnElementChanged(e);
+            UpdateDividerHeight();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            switch (e.PropertyName)
+            {
+                case nameof(CustomTableView.IsSeparatorVisible):
+                    UpdateDividerHeight();
+                    break;
+            }
+        }
+
+        private void UpdateDividerHeight()
+        {
+            Control.SeparatorStyle = (Element as CustomTableView)?.IsSeparatorVisible == false ? UITableViewCellSeparatorStyle.None : UITableViewCellSeparatorStyle.SingleLine;
+        }
+    }
+}
---
+++ src/KokoroIO.XamarinForms/Views/CustomTableView.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace KokoroIO.XamarinForms.Views
+{
+    public sealed class CustomTableView : TableView
+    {
+        #region IsSeparatorVisible
+
+        public static readonly BindableProperty IsSeparatorVisibleProperty
+            = BindableProperty.Create(nameof(IsSeparatorVisible), typeof(bool), typeof(CustomTableView), false);
+
+        public bool IsSeparatorVisible
+        {
+            get => (bool)GetValue(IsSeparatorVisibleProperty);
+            set => SetValue(IsSeparatorVisibleProperty, value);
+        }
+
+        #endregion IsSeparatorVisible
+    }
+}
