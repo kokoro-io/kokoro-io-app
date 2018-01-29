a3e95bd2ce686e4c368ada9391809c6ef1a2d70e
pgrho <shipwreck@live.jp>
2018-01-29 21:08:26 +0900

    Change  droid application label

--- src/KokoroIO.XamarinForms.Android/Secrets.cs
+++ src/KokoroIO.XamarinForms.Android/Secrets.cs
@@ -4,7 +4,14 @@ namespace KokoroIO.XamarinForms.Droid
     {
         // TODO: make environment variable
 
-        public const string SenderID = "323911141792";
+        public const string SenderID =
+#if DEBUG
+            "1001837084742"
+#else
+            "323911141792"
+#endif
+            ;
+
         public const string ListenConnectionString = "Endpoint=sb://kokoro-io.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=I0TdAKFvK2KWOZ+7a0v+rLklDjuK9KgNulwzgiobbuk=";
         public const string NotificationHubName = "kokoro-io-production";
     }
