b58a787ad1de6d976ccfdf643ef9bf85fba97b54
pgrho <shipwreck@live.jp>
2018-01-30 15:59:02 +0900

    Replace UIWebView by WKWebView

--- src/KokoroIO.XamarinForms.iOS/Renderers/MessagesViewRenderer.cs
+++ src/KokoroIO.XamarinForms.iOS/Renderers/MessagesViewRenderer.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Foundation;
 using KokoroIO.XamarinForms.iOS.Renderers;
 using KokoroIO.XamarinForms.Views;
-using UIKit;
+using WebKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -11,9 +11,9 @@ using Xamarin.Forms.Platform.iOS;
 
 namespace KokoroIO.XamarinForms.iOS.Renderers
 {
-    public sealed class MessagesViewRenderer : WebViewRenderer
+    public sealed class MessagesViewRenderer : ViewRenderer<MessagesView, WKWebView>
     {
-        private class CustomDelegate : UIWebViewDelegate
+        private class CustomDelegate : WKNavigationDelegate
         {
             private readonly MessagesViewRenderer _Renderer;
 
@@ -22,8 +22,17 @@ namespace KokoroIO.XamarinForms.iOS.Renderers
                 _Renderer = r;
             }
 
-            public override bool ShouldStartLoad(UIWebView webView, NSUrlRequest request, UIWebViewNavigationType navigationType)
-                => !_Renderer.OnNavigating(request.Url.ToString());
+            public override void DecidePolicy(WKWebView webView, WKNavigationAction navigationAction, Action<WKNavigationActionPolicy> decisionHandler)
+            {
+                if (_Renderer.OnNavigating(navigationAction.Request.Url.ToString()))
+                {
+                    decisionHandler(WKNavigationActionPolicy.Cancel);
+                }
+                else
+                {
+                    decisionHandler(WKNavigationActionPolicy.Allow);
+                }
+            }
         }
 
         private bool OnNavigating(string url)
@@ -40,33 +49,34 @@ namespace KokoroIO.XamarinForms.iOS.Renderers
             catch
             { }
 
-            //return true;
             return false;
         }
 
-        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        protected override void OnElementChanged(ElementChangedEventArgs<MessagesView> e)
         {
-            var mwv = e.OldElement as MessagesView;
-
-            if (mwv != null)
+            if (e.OldElement != null)
             {
-                mwv.UpdateAsync = null;
+                e.OldElement.UpdateAsync = null;
             }
 
             base.OnElementChanged(e);
-            Delegate = new CustomDelegate(this);
 
-            mwv = e.NewElement as MessagesView;
-
-            if (mwv != null)
+            if (Control == null)
             {
-                mwv.UpdateAsync = UpdateAsync;
+                SetNativeControl(new WKWebView(Frame, new WKWebViewConfiguration() { }));
+
+                Control.NavigationDelegate = new CustomDelegate(this);
+            }
+
+            if (e.NewElement != null)
+            {
+                e.NewElement.UpdateAsync = UpdateAsync;
             }
         }
 
         private bool _Loaded;
 
-        private Task<bool> UpdateAsync(bool reset, MessagesViewUpdateRequest[] requests, bool? hasUnread)
+        private async Task<bool> UpdateAsync(bool reset, MessagesViewUpdateRequest[] requests, bool? hasUnread)
         {
             var mwv = Element as MessagesView;
 
@@ -75,17 +85,17 @@ namespace KokoroIO.XamarinForms.iOS.Renderers
                 if (!_Loaded)
                 {
                     _Loaded = true;
-                    LoadHtmlString(MessagesViewHelper.GetHtml(), new NSUrl(NSBundle.MainBundle.BundlePath, true));
+                    Control.LoadHtmlString(MessagesViewHelper.GetHtml(), new NSUrl(NSBundle.MainBundle.BundlePath, true));
                 }
 
                 var script = MessagesViewHelper.CreateScriptForRequest(mwv.Messages, reset, requests, hasUnread);
                 if (!string.IsNullOrEmpty(script))
                 {
-                    EvaluateJavascript(script);
-                    return Task.FromResult(true);
+                    await Control.EvaluateJavaScriptAsync(script);
+                    return true;
                 }
             }
-            return Task.FromResult(false);
+            return false;
         }
     }
 }
