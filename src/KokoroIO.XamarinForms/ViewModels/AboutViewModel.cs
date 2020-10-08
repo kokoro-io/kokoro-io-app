d2e252225fdba09068e94ecc6bb0e02bb85146a9
pgrho <shipwreck@live.jp>
2020-10-08 16:59:51 +0900

    Add x:DataType directive

--- src/KokoroIO.XamarinForms/ViewModels/AboutViewModel.cs
+++ src/KokoroIO.XamarinForms/ViewModels/AboutViewModel.cs
@@ -1,42 +1,27 @@
-using System;
 using System.Collections.ObjectModel;
 
 namespace KokoroIO.XamarinForms.ViewModels
 {
     public sealed class AboutViewModel : BaseViewModel
     {
-        public class DependentLibrary
-        {
-            public DependentLibrary(string displayName, string url)
-            {
-                DisplayName = displayName;
-                Url = url;
-            }
+        private ReadOnlyCollection<DependentLibraryViewModel> _Dependencies;
 
-            public string DisplayName { get; }
-            public string Url { get; }
-        }
-
-        private ReadOnlyCollection<DependentLibrary> _Dependencies;
-
-        public ReadOnlyCollection<DependentLibrary> Dependencies
+        public ReadOnlyCollection<DependentLibraryViewModel> Dependencies
         {
             get
             {
                 if (_Dependencies == null)
                 {
-                    _Dependencies = new ReadOnlyCollection<DependentLibrary>(new[]
+                    _Dependencies = new ReadOnlyCollection<DependentLibraryViewModel>(new[]
                     {
-                        new  DependentLibrary("XLabs", "https://github.com/XLabs/Xamarin-Forms-Labs"),
-                        new  DependentLibrary("Material icons", "https://material.io/icons/"),
-                        new  DependentLibrary("Bootstrap 3.3.7", "https://getbootstrap.com/docs/3.3/"),
-                        new  DependentLibrary("portable-exif-lib", "https://github.com/ravensorb/portable-exif-lib"),
+                        new  DependentLibraryViewModel("XLabs", "https://github.com/XLabs/Xamarin-Forms-Labs"),
+                        new  DependentLibraryViewModel("Material icons", "https://material.io/icons/"),
+                        new  DependentLibraryViewModel("Bootstrap 3.3.7", "https://getbootstrap.com/docs/3.3/"),
+                        new  DependentLibraryViewModel("portable-exif-lib", "https://github.com/ravensorb/portable-exif-lib"),
                     });
-
                 }
                 return _Dependencies;
             }
         }
-
     }
 }
---
+++ src/KokoroIO.XamarinForms/ViewModels/DependentLibraryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace KokoroIO.XamarinForms.ViewModels
+{
+    public sealed class DependentLibraryViewModel
+    {
+        internal DependentLibraryViewModel(string displayName, string url)
+        {
+            DisplayName = displayName;
+            Url = url;
+        }
+
+        public string DisplayName { get; }
+        public string Url { get; }
+    }
+}
